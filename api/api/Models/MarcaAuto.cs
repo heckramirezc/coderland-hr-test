c08fc4425b03c052834fdab81cd2059a77777c2f
Hector Ramírez <heckramirezc@aol.com>
2025-11-04 03:16:53 -0600

    Configuración de data seed para listado inicial de marcas

--- api/api/Models/MarcaAuto.cs
+++ api/api/Models/MarcaAuto.cs
@@ -4,17 +4,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api.Models
 {
-    [Table("MarcasAutos")]
     public class MarcaAuto
     {
-        [Key]
         public int Id { get; set; }
-
-        [Required]
-        [MaxLength(100)]
-        public required string Nombre { get; set; }
-
-        [MaxLength(50)]
-        public string? PaisOrigen { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string PaisOrigen { get; set; } = null!;
     }
 }
