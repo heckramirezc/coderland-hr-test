23bec971017fe06a97000d504b1957f490a10013
Hector Ramírez <heckramirezc@aol.com>
2025-11-04 03:52:57 -0600

    Migración y actualización Data seed

---
+++ api/api/Migrations/20251104094644_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MarcasAutos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    PaisOrigen = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MarcasAutos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "MarcasAutos",
+                columns: new[] { "Id", "Nombre", "PaisOrigen" },
+                values: new object[,]
+                {
+                    { 1, "Toyota", "Japón" },
+                    { 2, "Ford", "Estados Unidos" },
+                    { 3, "BMW", "Alemania" },
+                    { 4, "Honda", "Japón" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MarcasAutos");
+        }
+    }
+}
--- api/api/Program.cs
+++ api/api/Program.cs
@@ -1,4 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString)
+);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -10,7 +18,18 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        db.Database.Migrate();
+    }
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
 
 app.UseHttpsRedirection();
 
@@ -19,4 +38,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
