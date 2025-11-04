118f732acab449e3dcbfd6fa46a65d8ca3d06b28
Hector Ramírez <heckramirezc@aol.com>
2025-11-03 20:03:17 -0600

    Implementación de App.tsx y HomeScreen.tsx con los dos botones de navegación.

---
+++ app/src/screens/Home/HomeScreen.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import {RootStack } from '../../types/navigation';
+
+type Props = NativeStackScreenProps<RootStack, 'Home'>;
+
+const HomeScreen: React.FC<Props> = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bienvenido</Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Tasks"
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Listado"
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 40,
+  },
+  buttonContainer: {
+    width: '80%',
+    marginBottom: 20,
+  },
+});
+
+export default HomeScreen;
---
+++ app/src/types/navigation.ts
@@ -0,0 +1,5 @@
+export type RootStack = {
+  Home: undefined;
+  Tasks: undefined;
+  List: undefined;
+};
