30659bff43db27f9827c45f995a67a0ebfa6fb06
Hector Ramírez <heckramirezc@aol.com>
2025-11-04 02:08:55 -0600

    Mejoras en navegación

--- app/App.tsx
+++ app/App.tsx
@@ -1,28 +1,20 @@
-// App.tsx
-
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import HomeScreen from './src/screens/Home/HomeScreen';
-import TasksScreen from './src/screens/Tasks/TasksScreen';
-import ListScreen from './src/screens/List/ListScreen';
-import { RootStack } from './src/types/navigation';
 import { Provider } from 'react-redux';
-import { store } from './src/store/store'; 
+import { NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'; // <-- IMPORTANTE
+import { store } from './src/store/store';
+import AppNavigator from './src/navigation/AppNavigator';
 
-const Stack = createNativeStackNavigator<RootStack>();
-
-export default function App() {
+const App: React.FC = () => {
   return (
-    <Provider store={store}> 
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Principal' }} />
-          <Stack.Screen name="Tasks" component={TasksScreen} options={{ title: 'Tasks' }} />
-          <Stack.Screen name="List" component={ListScreen} options={{ title: 'Listado' }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Provider store={store}>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </Provider>
+    </GestureHandlerRootView>
   );
-}
+};
+
+export default App;
---
+++ app/src/navigation/AppNavigator.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { RootStack } from '../types/navigation';
+import HomeScreen from '../screens/Home/HomeScreen';
+import TasksScreen from '../screens/Tasks/TasksScreen';
+import ListScreen from '../screens/List/ListScreen';
+
+const Stack = createStackNavigator<RootStack>();
+
+const AppNavigator: React.FC = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen 
+        name="Home" 
+        component={HomeScreen} 
+        options={{ title: 'Home', headerShown: false }}
+      />
+      <Stack.Screen 
+        name="Tasks" 
+        component={TasksScreen} 
+        options={{ title: 'Tasks' }} 
+      />
+      <Stack.Screen 
+        name="List" 
+        component={ListScreen} 
+        options={{ title: 'Listado' }} 
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default AppNavigator;
