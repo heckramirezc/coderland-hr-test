3a68dc0a223337a2d189e8c01dcc1b92cb8a83a9
Hector Ramírez <heckramirezc@aol.com>
2025-11-04 03:19:30 -0600

    Agregar controlador con endpoint GET para obtener marcas de autos

---
+++ api/api/Controllers/Autos/autosController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using api.Data;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers.Autos
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class autosController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public autosController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IEnumerable<MarcaAuto>>> GetAll()
+        {
+            var brands = await _db.MarcasAutos.ToListAsync();
+            return Ok(brands);
+        }
+    }
+}
